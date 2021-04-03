4cf6a940a79c4688ce0bf263d007b1750eedad27
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-04-03 21:44:46 +0300

    Sum of numbers

--- Problems/The sign of a number/src/Main.java
+++ Problems/The sign of a number/src/Main.java
@@ -7,13 +7,14 @@ public class Main {
 
         if (number > 0) {
             return 1;
-        }else if (number<0) {
+        } else if (number < 0) {
             return -1;
-        }else if (number == 0){
+        } else if (number == 0) {
             return 0;
         }
         return number;
     }
+
     /* Do not
 change code below */
     public static void main(String[] args) {
