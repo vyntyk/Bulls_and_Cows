d0da94cfd461ad6c0346cb8257d64f6cc331c451
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-04-04 23:37:04 +0300

    Calculator

---
+++ Problems/Calculator with four methods/src/Main.java
@@ -0,0 +1,24 @@
+import static java.lang.Math.*;
+
+class SimpleCalculator {
+    // Implement your methods here
+    public static void subtractTwoNumbers(long a, long b){
+        System.out.println(a - b);
+    }
+
+    public static void sumTwoNumbers(long a, long b){
+        System.out.println(a + b);
+    }
+
+    public static void divideTwoNumbers(long a, long b){
+        if(b!=0){
+            System.out.println(a/b);
+        }else{
+            System.out.println("Division by 0!");
+        }
+    }
+
+    public static void multiplyTwoNumbers(long a, long b){
+        System.out.println(a * b);
+    }
+}
