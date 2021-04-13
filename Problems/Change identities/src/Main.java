b16041e9cf42321368c4653bca6eafa4291ee0ac
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-04-13 22:27:30 +0300

    Change identities

---
+++ Problems/Change identities/src/Main.java
@@ -0,0 +1,18 @@
+class Person {
+    String name;
+    int age;
+}
+
+class MakingChanges {
+    public static void changeIdentities(Person p1, Person p2) {
+        // write your code here
+        Person temp = new Person();
+
+        temp.name = p1.name;
+        temp.age = p1.age;
+        p1.name = p2.name;
+        p1.age = p2.age;
+        p2.name = temp.name;
+        p2.age = temp.age;
+    }
+}
